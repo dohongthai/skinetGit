71d846b469ed61a9fbc27d4d42542d08f4fb7bb6
dohongthai <76418150+dohongthai@users.noreply.github.com>
2021-05-27 09:08:42 +0700

    Lỗi sữa mãi mới được =))))

--- API/Startup.cs
+++ API/Startup.cs
@@ -38,7 +38,9 @@ namespace API
             {
                 otp.AddPolicy("CorsPolicy", policy =>
             {
-                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                policy.AllowAnyHeader()
+                .AllowAnyMethod()
+                .WithOrigins("http://localhost:4200");
             });
             });
         }
